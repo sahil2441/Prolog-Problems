2d0998661be5f89ab6e71467bc54638c2f679ed1
Sahil Jain <sahil.2441@gmail.com>
2015-10-15 01:24:56 -0400

    Assignment 2 Questio one completedgit status

--- Second/question1.pl
+++ Second/question1.pl
@@ -2,13 +2,19 @@
 %The idea is to generate all permutations of the given operating sequence
 %and store result form each one of them into a list and then find the max 
 %element of that list.
-%[hw2_1].
 % maxval(tree(1,2),[minus],Max).
 
 maxval(tree(X,Y),L,Max):-
+	findall(M, startmaxval(tree(X,Y),L,M), Collections),
+	quick_sort(Collections,SortedCollection),
+	rev(SortedCollection,ReverseCollection),
+	ReverseCollection= [H1|_],
+	Max=H1.
+
+startmaxval(tree(X,Y),L,M):-
 	permute(L,L1),
-	evaluate(tree(X,Y),L1,Max),
-	
+	evaluate(tree(X,Y),L1,M).
+
 
 %Spliting the evaluate fuction into four cases
 %when both sub trees are atoms
@@ -27,9 +33,8 @@ evaluate(tree(X,Y),L,Max):-
 			Max is X-Y
 		; H == 'mult' ->
 			Max is X*Y
-	),
-	!.
-
+	).
+	
 %part2
 evaluate(tree(X,Y),L,Max):-
 	L=[H|T],
@@ -42,8 +47,8 @@ evaluate(tree(X,Y),L,Max):-
 			Max is X-M1
 		; H == 'mult' ->
 			Max is X*M1
-	),
-	!.
+	).
+	
 
 %part3
 evaluate(tree(X,Y),L,Max):-
@@ -57,9 +62,8 @@ evaluate(tree(X,Y),L,Max):-
 			Max is M1-Y
 		; H == 'mult' ->
 			Max is M1*Y
-	),
-	!.
-
+	).
+	
 %FinalPart
 %We need to keep H and split T between two subtrees
 evaluate(tree(X,Y),L,Max):-
@@ -79,9 +83,7 @@ evaluate(tree(X,Y),L,Max):-
 
 	evaluate(X,L1,R1),
 	evaluate(Y,L2,R2),
-	evaluate(tree(R1,R2),[H],Max),
-	
-	!.
+	evaluate(tree(R1,R2),[H],Max).
 
 %Helper
 integers(tree(X,Y),2):-
@@ -141,6 +143,43 @@ permute([H|T],L):-
 	permute(T,L1),
 	delete1(L,H,L1).
 
+factorial(0,1).
+factorial(N,F):-
+	M is N-1,
+	factorial(M,F1),
+	F is N*F1.
+
+max(X,Y,Y)  :- 
+	 number(Y),
+	 X  =<  Y,!. 
+max(X,_,X).
+
+naiveSort(X,L):-
+	permute(X,L),
+	isSorted(L).
+
+isSorted([]).
+isSorted([_]).
+isSorted([H|T]):-
+	T = [A|_],
+	H=<A,
+	isSorted(T).
+
+quick_sort([],[]).
+quick_sort([H|T],L):-
+	pivoting(H,T,L1,L2),
+	quick_sort(L1,Result1),
+	quick_sort(L2,Result2),
+	append(Result1,[H|Result2],L).
+
+pivoting(_,[],_,_).
+pivoting(H,[A|T],[A|L1],L2):-
+	A=<H,
+	pivoting(H,T,L1,L2).
+
+pivoting(H,[A|T],L1,[A|L2]):-
+	A>H,
+	pivoting(H,T,L1,L2).
 
 
 
