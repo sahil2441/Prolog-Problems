f2f661822109d3099930b5f9edeaec4bac4a9278
Sahil Jain <sahil.2441@gmail.com>
2015-11-22 21:36:29 -0500

    N queens finished

--- 6/fqueens.pl
+++ 6/fqueens.pl
@@ -17,12 +17,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-board_size(4).
-pos(1,2).
-pos(1,3).
-pos(2,1). 
-pos(4,3). 
-
+board_size(5).
+pos(1,1).
+pos(2,5).
+pos(3,3).
+pos(5,2).
+pos(5,5).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%% HELPER FUNCTIONS
@@ -33,15 +33,12 @@ delete1([H|T],H,T).
 delete1([H|Ys],X,[H|Zs]):-
 	delete1(Ys,X,Zs).
 
+append1(A-B, B-C, A-C).
+
 permute([],[]).
 permute([H|T],L):-
 	permute(T,L1),
 	delete1(L,H,L1).
-
-
-append([],L,L).
-append([H|T],X,[H|Y]):-
-	append(T,X,Y).
 
 get_List(0,[]):-!.
 get_List(N,[N|T]):-
@@ -77,7 +74,21 @@ minl([Head|Tail], Minimum) :-
 	minl(Tail, TailMin),
 	Minimum is min(Head, TailMin).
 
+length1([], 0).
+length1([_|Xs], M):-
+	length1(Xs, N),
+	M is N+1.
 
+remove_duplicates([],[]):- !.
+remove_duplicates([H|T],R):- member(H,T),remove_duplicates(T,R),!.
+remove_duplicates([_|T],[_|Rest]):- remove_duplicates(T,Rest).
+
+print_permutations([]):-!.
+print_permutations([H|T]):-
+	write(H),
+	nl,
+	print_permutations(T),
+	!.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -87,10 +98,16 @@ minl([Head|Tail], Minimum) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fqueens(X):-
+moves(X):-
 	findall(A,solve(A),L),
+%	write("All Solutions: "), write(L),nl,
+%	length1(L,L2),
+%	write("No of Solutions: "), write(L2),nl,
 	convert(L,L1),
+%	print_permutations(L1),
 	get_all_permutations(L1,Permutations),
+	length1(Permutations,N1),
+%	write("No of Permutations: "), write(N1),nl ,
 	findall((X,Y),pos(X,Y),INPUT),
 	findall(D,get_all_distances(Permutations,INPUT,D),Distances),
 	minl(Distances,X),
@@ -114,15 +131,17 @@ convert_helper(I,[H|T],R):-
 	append(R1,[A],R),
 	!.
 
-get_all_permutations([],[]).
+get_all_permutations([],[]):-!.
 get_all_permutations([H|T],R):-
-	findall(X,permute(H,X),R1),
-	append(R2,R1,R),
-	get_all_permutations(T,R2).
+	findall(X,permutation(H,X),R1),
+	append(R1,R2,R),
+	get_all_permutations(T,R2),
+	!.
 	
 get_all_distances(L,INPUT,D):-
 	member(X,L),
 	calculate_distance(X,INPUT,D).
+%	write(X), write(" : "),write(INPUT),write(" : "), write(D), nl.
 
 calculate_distance([],[],0):-!.
 calculate_distance([H1|T1],[H2|T2],D):-
@@ -130,17 +149,10 @@ calculate_distance([H1|T1],[H2|T2],D):-
 	H2=(X2,Y2),
 	DX is abs(X1-X2),
 	DY is abs(Y1-Y2),
-	D1 is DX+DY,
+	D1 is max(DX,DY),
 	calculate_distance(T1,T2,D2),
 	D is D1+D2,
 	!.
-
-
-
-
-
-
-
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -152,15 +164,19 @@ calculate_distance([H1|T1],[H2|T2],D):-
 
 test(X):-	
 %	get_input(INPUT),
-%	getX(L),
-%	calculate_distance(L,INPUT,X).
-	fqueens(X).
+%	get_all_permutations(INPUT,P),
+%	length1(P,X).
+	moves(X).
 
 get_input(INPUT):-
-	INPUT = [ (1, 2), (2, 4), (3, 1), (4, 3)].
+	INPUT =[
+	[ (6,3), (5,6), (4,2), (3,5), (2,1), (1,4)],
+[ (6,2), (5,4), (4,6), (3,1), (2,3), (1,5)],
+[ (6,5), (5,3), (4,1), (3,6), (2,4), (1,2)],
+[ (6,4), (5,1), (4,5), (3,2), (2,6), (1,3)]
+].
+
 
 getX(X):-
-	X = [ (4, 2), (2, 3), (3, 1), (1, 3)].
-
-
+	X =  [ (1,2), (2,4), (3,1), (4,3)].
 
